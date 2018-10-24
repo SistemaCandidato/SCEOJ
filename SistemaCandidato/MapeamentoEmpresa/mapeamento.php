37ac5b5989ea9682c0d5bab57926eaffc2032459
viniciusnazario <USURIO@USURIO-PC>
2018-10-24 20:32:38 -0300

    Select pra buscar de candidatos, nesse primeiro momento irei trabalhar com 3 selects diferentes, porém, acredito que fique muito ruim, assim que testar mais coisas tento fazer os 3 num só

--- SistemaCandidato/MapeamentoEmpresa/mapeamento.php
+++ SistemaCandidato/MapeamentoEmpresa/mapeamento.php
@@ -1,21 +1,60 @@
+<meta charset="UTF-8">
 <?php
 include '../sql/conectar.php';
+include '../cabecalho.php';
+ $id = $_GET['id'];
+ 
+ 
+
+
+ 
+
 $Areas_id = str_replace(" ", "%", $_GET['q']);
 
-$sql  = "SELECT candidatos.nome,
-       conhecimentos.nome,
-       cursos.nome,
-       trabalhos.nome
-FROM candidatos
-INNER JOIN conhecimentos ON candidatos.id = conhecimentos.candidatos_id
-INNER JOIN cursos ON candidatos.id = cursos.candidatos_id
-INNER JOIN trabalhos ON candidatos.id = trabalhos.candidatos_id
-WHERE candidatos.datainclusao >= DATE_FORMAT(NOW(), '%Y-%m-%d')
-AND (candidatos.nome LIKE '%TI%'
-       OR conhecimentos.nome LIKE '%TI%'
-       OR cursos.nome LIKE '%TI%'
-       OR trabalhos.nome LIKE '%TI%')";
+$resultado  = ("select candidatos.id,candidatos.nome AS candidato,conhecimentos.nome FROM candidatos INNER JOIN conhecimentos WHERE candidatos.id = conhecimentos.candidatos_id AND conhecimentos.nome LIKE '%$Areas_id%'");
+$oi = mysqli_query($conexao, $resultado);
 
-
-$resultado = mysqli_query($conexao, $sql);
+if($oi != NULL){
 ?>
+ <meta charset="UTF-8">
+<br><br><br><br><br><br><br><br>
+<div class="container">
+  <p>Escolha uma vaga para selecionar o candidato</p>                                                                                      
+  <div class="table-responsive">          
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Nome candidato</th>
+        <th>Conhecimentos</th>
+        <th>Cursos</th>
+        <th>Trabalhos</th>
+        <th>Mostrar interesse</th>
+    
+        
+      </tr>
+    </thead>
+<?php
+    while ($linha = mysqli_fetch_array($oi)) {
+   ?>
+        <tr>
+       
+            <td><?= $linha['candidato']?></td> 
+            <td><?= $linha['nome'] ?></td>
+           
+                        
+        </tr>
+        </table>
+  </div>
+        <?php 
+    }
+    }
+    ?>
+
+
+  
+
+    
+
+
+
+    
