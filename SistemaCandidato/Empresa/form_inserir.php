254c7348fb866f24d830b1a5b15f849899f5ee2b
Octavio1Naspolini <>
2018-11-18 17:43:35 -0200

    Merge origin/master

--- SistemaCandidato/Empresa/form_inserir.php
+++ SistemaCandidato/Empresa/form_inserir.php
@@ -128,8 +128,8 @@ endif;
     <div id="signupbox" style=" margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info">
             <div class="panel-heading">
-                <div class="panel-title">Sign Up</div>
-                <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="../login/form_login.php">Sign In</a></div>
+                <div class="panel-title">Cadastre-se</div>
+                <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="../login/form_login.php">Já tem cadastro? Então, entre</a></div>
             </div>  
             <div class="panel-body" >
                 <form method="post" action="#">
