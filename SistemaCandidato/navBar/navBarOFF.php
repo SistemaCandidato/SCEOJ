d53c6430b71de2562f2ef2c0fdbb74a530961eb3
Octavio1Naspolini <42946359+Octavio1Naspolini@users.noreply.github.com>
2018-11-19 14:45:40 -0200

    Update navBarOFF.php

--- SistemaCandidato/navBar/navBarOFF.php
+++ SistemaCandidato/navBar/navBarOFF.php
@@ -2,7 +2,7 @@
 <html lang="pt">
 
   <head>
-
+    <link rel="icon" href="img/icone.png">  
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
