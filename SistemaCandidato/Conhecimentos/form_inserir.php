cdda60902cfddc5304c84b664ab0ace156eb02bd
Octavio1Naspolini <42946359+Octavio1Naspolini@users.noreply.github.com>
2018-11-20 08:29:09 -0200

    Update form_inserir.php

--- SistemaCandidato/Conhecimentos/form_inserir.php
+++ SistemaCandidato/Conhecimentos/form_inserir.php
@@ -25,6 +25,7 @@ and open the template in the editor.
         <div id="signupbox" style=" margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info">
             <div class="panel-heading">
+             <div class="panel-title">Cadastre-se</div>
                 <div style="float:right; font-size: 85%; position: relative; top:-10px"></div>
             </div>  
             <div class="panel-body" >
