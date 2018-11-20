846f4b36cbe6e9fbc4d00311eee2edff28695dbd
Octavio1Naspolini <42946359+Octavio1Naspolini@users.noreply.github.com>
2018-11-20 08:26:20 -0200

    Update form_inserir.php

--- SistemaCandidato/vagas/form_inserir.php
+++ SistemaCandidato/vagas/form_inserir.php
@@ -28,7 +28,7 @@ and open the template in the editor.
     <div id="signupbox" style=" margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info">
             <div class="panel-heading">
-                <div class="panel-title">Sign Up</div>
+                <div class="panel-title">Cadastre-se</div>
                 
             </div>  
             <div class="panel-body" >
