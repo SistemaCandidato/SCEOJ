e2051aa5a46f93c928a4e5675885dd6ddec5e1c1
Octavio1Naspolini <42946359+Octavio1Naspolini@users.noreply.github.com>
2018-11-27 15:59:01 -0200

    Mudanças no banner

--- SistemaCandidato/navBar/navBarOFF.php
+++ SistemaCandidato/navBar/navBarOFF.php
@@ -54,7 +54,7 @@
     </nav>
 
 
-  <header class="masthead" style="background-image: url('img/capa.png')">
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
       <div class="overlay"></div>
       <div class="container">
         <div class="row">
